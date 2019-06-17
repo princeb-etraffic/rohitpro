b1c520e025aaae59d002dc185c259950b790b791
prince <princeb.etraffic@gmail.com>
2019-06-17 16:19:48 +0530

    Updates

---
+++ precache-manifest.9c686f74a297108d22af7ef5ec50aa1d.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f744f9d29a0f909585b25363982babbc",
+    "url": "/rohitpro/index.html"
+  },
+  {
+    "revision": "d6355c664ec8778938de",
+    "url": "/rohitpro/static/css/main.30f3f143.chunk.css"
+  },
+  {
+    "revision": "59b578d8fee95fb4bef8",
+    "url": "/rohitpro/static/js/2.c7924f40.chunk.js"
+  },
+  {
+    "revision": "d6355c664ec8778938de",
+    "url": "/rohitpro/static/js/main.6298dc64.chunk.js"
+  },
+  {
+    "revision": "aca99d25c7933b066c64",
+    "url": "/rohitpro/static/js/runtime~main.b08be204.js"
+  },
+  {
+    "revision": "89057044359d9e8f59ff03d92fb477a7",
+    "url": "/rohitpro/static/media/PSD.89057044.png"
+  },
+  {
+    "revision": "b0ea2492c622804601be69f9dea95636",
+    "url": "/rohitpro/static/media/banner-bg.b0ea2492.jpg"
+  },
+  {
+    "revision": "97013f2cf49f902a96f04a2a20b210a7",
+    "url": "/rohitpro/static/media/css.97013f2c.png"
+  },
+  {
+    "revision": "a21a777574898c2940133756363a7df4",
+    "url": "/rohitpro/static/media/html.a21a7775.png"
+  },
+  {
+    "revision": "5a300df956916e50ac8d2d5109818bb5",
+    "url": "/rohitpro/static/media/icon-gmail.5a300df9.svg"
+  },
+  {
+    "revision": "8f377fbcca15503529265758362def00",
+    "url": "/rohitpro/static/media/icon-linkedin.8f377fbc.svg"
+  },
+  {
+    "revision": "0a8842b0d075d645a56fdcba6a6e70d4",
+    "url": "/rohitpro/static/media/icon-skype.0a8842b0.svg"
+  },
+  {
+    "revision": "4c3c60e700a4a5e4746d3ee5f8d52136",
+    "url": "/rohitpro/static/media/icon-whatsapp.4c3c60e7.svg"
+  },
+  {
+    "revision": "9278e3baffff4a28d657fc6d5537a856",
+    "url": "/rohitpro/static/media/js.9278e3ba.png"
+  },
+  {
+    "revision": "227aff05218583136210ad2ea7e86455",
+    "url": "/rohitpro/static/media/portfolio-1.227aff05.png"
+  },
+  {
+    "revision": "297c983b282dcc37555d7f76330f84e4",
+    "url": "/rohitpro/static/media/portfolio-10.297c983b.jpg"
+  },
+  {
+    "revision": "699bfbea9862c2e9054c44d2a18039e6",
+    "url": "/rohitpro/static/media/portfolio-10.699bfbea.jpg"
+  },
+  {
+    "revision": "0c19ff29b192db2cd5cc697456866a20",
+    "url": "/rohitpro/static/media/portfolio-11.0c19ff29.jpg"
+  },
+  {
+    "revision": "8e1c2a9cc59af5080e752f9ccdac7dca",
+    "url": "/rohitpro/static/media/portfolio-11.8e1c2a9c.jpg"
+  },
+  {
+    "revision": "2112d9e8b1194ddfd88dc1f49b1f1010",
+    "url": "/rohitpro/static/media/portfolio-12.2112d9e8.jpg"
+  },
+  {
+    "revision": "b7451556ca866c54ddebac1c8885ce75",
+    "url": "/rohitpro/static/media/portfolio-12.b7451556.jpg"
+  },
+  {
+    "revision": "081a2ac7c29ca022aaa369eae1557e91",
+    "url": "/rohitpro/static/media/portfolio-2.081a2ac7.jpg"
+  },
+  {
+    "revision": "e2ac7eafb12899c60d43a66fdc56d4e7",
+    "url": "/rohitpro/static/media/portfolio-2.e2ac7eaf.jpg"
+  },
+  {
+    "revision": "0bb4492302aee3278f5953fc51627f47",
+    "url": "/rohitpro/static/media/portfolio-3.0bb44923.jpg"
+  },
+  {
+    "revision": "c17997e4c7b86862206c00232113180d",
+    "url": "/rohitpro/static/media/portfolio-3.c17997e4.jpg"
+  },
+  {
+    "revision": "6801909c5e025fd60b7e9da550738289",
+    "url": "/rohitpro/static/media/portfolio-4.6801909c.jpg"
+  },
+  {
+    "revision": "da7173b01df6ff587b54a4525075a6ef",
+    "url": "/rohitpro/static/media/portfolio-4.da7173b0.jpg"
+  },
+  {
+    "revision": "0581a933079a0f9ab340b449d6a9f465",
+    "url": "/rohitpro/static/media/portfolio-5.0581a933.jpg"
+  },
+  {
+    "revision": "6783759dd89c59529741d4aafe39a773",
+    "url": "/rohitpro/static/media/portfolio-5.6783759d.jpg"
+  },
+  {
+    "revision": "4ad3c457a85aec3e4bf4ca63d4ead417",
+    "url": "/rohitpro/static/media/portfolio-6.4ad3c457.jpg"
+  },
+  {
+    "revision": "ae3589d290136609d64d4345a302457e",
+    "url": "/rohitpro/static/media/portfolio-6.ae3589d2.jpg"
+  },
+  {
+    "revision": "25ba2217d320bdb91b9271d11a65e2f0",
+    "url": "/rohitpro/static/media/portfolio-7.25ba2217.jpg"
+  },
+  {
+    "revision": "c2094ed131d06a83591e9c60d38fa3da",
+    "url": "/rohitpro/static/media/portfolio-7.c2094ed1.jpg"
+  },
+  {
+    "revision": "0c6291c8b690de3dfcf3c0debc100a63",
+    "url": "/rohitpro/static/media/portfolio-8.0c6291c8.jpg"
+  },
+  {
+    "revision": "38453d10bde7038fff719d6e6a21aa18",
+    "url": "/rohitpro/static/media/portfolio-8.38453d10.jpg"
+  },
+  {
+    "revision": "c7d664c7f69bf5cd87cbc9d57488379f",
+    "url": "/rohitpro/static/media/portfolio-9.c7d664c7.jpg"
+  },
+  {
+    "revision": "e4898f7e1eb77f47db7f7bc50b58fbc4",
+    "url": "/rohitpro/static/media/portfolio-9.e4898f7e.jpg"
+  },
+  {
+    "revision": "82ca1032f484bf514a224f0744c271c7",
+    "url": "/rohitpro/static/media/shopify.82ca1032.png"
+  },
+  {
+    "revision": "6eb2ca9a98a5321187d0bfeae15a9acb",
+    "url": "/rohitpro/static/media/squadspace.6eb2ca9a.png"
+  },
+  {
+    "revision": "4be2150b59a99150352b770cef2b1e31",
+    "url": "/rohitpro/static/media/wordpress.4be2150b.png"
+  }
+]);
